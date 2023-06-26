1588d6f18e2e5e5f18235f6444062df2fb90e606
mikkoki <mikko.kiviniemi@brightstraining.com>
2023-06-26 22:54:42 +0300

    Play with pet feature closes #3
    -Adding feature that you can play with pet

--- includes/utils.h
+++ includes/utils.h
@@ -14,7 +14,10 @@ struct Pet {
 
 //Feed the pet, which increases the pet's happiness and fullness.
 void feed_pet (Pet& pet, int amount);
+
 //Play with the pet, which increases the pet's happiness but decreases it's energy
+void play_pet(Pet &pet, int amount);
+
 //Put the pet to sleep, which increases its energy
 //Display the pet's current stats (happiness, fullness and energy)
 //Save the pet's current state to a file
