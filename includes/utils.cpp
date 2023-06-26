cadadb155d0ef29ce13b689d2aaadf5509288856
mikkoki <mikko.kiviniemi@brightstraining.com>
2023-06-26 22:47:16 +0300

    Add feed feature
    -Adding feeding feature

--- includes/utils.cpp
+++ includes/utils.cpp
@@ -1,1 +1,8 @@
 #include "utils.h"
+
+
+//Feed the pet, which increases the pet's happiness and fullness.
+void feed_pet (Pet& pet, int amount) {
+    pet.energy += amount;
+    pet.happiness += amount;
+}
